9dee1b42617c6aa857445dd78de642ee5cba56ea
szoon2426 <s.zoon2426@gmail.com>
2025-04-20 00:28:55 +0900

    주문페이지 동적 기능 추가 + 메뉴 사진 추가

--- js/payment.js
+++ js/payment.js
@@ -1,13 +1,13 @@
 const prices = {
   set: { single: 3500, drink: 4200, fry: 6000 },
-  single: { beef: 500, chicken: 0, mixed: 0, spicyChicken: 500, cutlet: 1000, tender: 1000 },
+  single: { beef: 500, chicken: 0, mix: 0, spicyChicken: 500, cutlet: 1000, tender: 1000 },
   drink: { coke: 0, zerocoke: 0, sprite: 0, beer: 1500, cafri: 2000, budweiser:2500 },
   fry: { frenchfries: 0, onionfries: 0, seaweed: 0 }
 };
 
 const map = {
   set: {single : "단품 부리또", drink : "음료세트" , fry : "튀김세트"},
-  single: { beef: "소고기", chicken: "닭고기", mixed: '믹스', spicyChicken: '닭갈비', cutlet: '돈가스', tender: '치킨텐더' },
+  single: { beef: "소고기", chicken: "닭고기", mix: '믹스', spicyChicken: '닭갈비', cutlet: '돈가스', tender: '치킨텐더' },
   drink: { coke: '콜라', zerocoke: '제로콜라', sprite: '스프라이트', beer: '맥주', cafri: '카프리', budweiser:'버드와이저' },
   fry: { frenchfries: '감자튀김', onionfries: '양파튀김', seaweed: '김말이튀김' }
 }
